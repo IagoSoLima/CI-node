0e506afe38c6999dd2732dc2501a98e86d2219c4
Iago So Lima <iago.souza069@gmail.com>
2023-01-28 20:51:37 -0300

    test: multiplicatication and division

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const sum = require("./sum");
 const subtract = require("./subtract");
+const multiplication = require("./multiplication");
+const division = require("./division");
 
 function app() {
   const n1 = 4;
@@ -8,6 +10,8 @@ function app() {
   console.log(`numbers: ${n1} e ${n2}`);
   console.log("sum: ", sum(n1, n2));
   console.log("subtract: ", subtract(n1, n2));
+  console.log("multiplication: ", multiplication(n1, n2));
+  console.log("division: ", division(n1, n2));
 }
 
 module.exports = app;
