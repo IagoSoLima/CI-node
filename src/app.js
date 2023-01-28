bfff27417a87b063b2273dfd84c0c8343e35c592
Iago So Lima <iago.souza069@gmail.com>
2023-01-28 16:09:59 -0300

    chore: add sonarqube config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
-const subtract = require("./subtract");
-const sum = require("./sum");
+function app() {
+  const subtract = require("./subtract");
+  const sum = require("./sum");
 
-const n1 = 4;
-const n2 = 5;
+  const n1 = 4;
+  const n2 = 5;
 
-console.log(`numbers: ${n1} e ${n2}`);
-console.log("sum: ", sum(n1, n2));
-console.log("subtract: ", subtract(n1, n2));
+  console.log(`numbers: ${n1} e ${n2}`);
+  console.log("sum: ", sum(n1, n2));
+  console.log("subtract: ", subtract(n1, n2));
+}
+
+module.exports = app;
+
+app();
