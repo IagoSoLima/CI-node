ed0dd29dbad97d9e2011d600ff5d6e91e663dcaa
Iago So Lima <iago.souza069@gmail.com>
2023-01-31 16:55:57 -0300

    chore: check sonnarcloud quality gate

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ function app() {
   const n2 = 5;
 
   console.log(`numbers: ${n1} e ${n2}`);
-  console.log("sum: ", sum(n1, n2));
-  console.log("subtract: ", subtract(n1, n2));
+  // console.log("sum: ", sum(n1, n2));
+  // console.log("subtract: ", subtract(n1, n2));
   // console.log("multiplication: ", multiplication(n1, n2));
 
-  console.log("division: ", division(n1, n2));
+  // console.log("division: ", division(n1, n2));
 }
 
 module.exports = app;
